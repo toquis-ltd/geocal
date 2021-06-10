44e38bcd5b53b42fdf17c3f82077e12fb0adaae4
Jakob OGANESYAN <jakoganesyan@gmail.com>
2021-06-10 15:54:29 +0200

    integrate sources app to webcms

--- webcms-server/app/sources/urls.py
+++ webcms-server/app/sources/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 app_name = "sources"
 urlpatterns = [
-    path('', sites_list, name='links'),
+    path('', sites_list, name=''),
     path('suggest/', contact_form, name='suggest'),
-    # path('report/', contact_form, name='report'),
-    # path('add/<str:context>', add, name='add'),
+    path('report/<int:id>/', contact_form, name='report'),
+    path('add/<str:context>/', add, name='add'),
     # path('download/', download, name='download')
 ]
--- webcms-server/app/sources/views.py
+++ webcms-server/app/sources/views.py
@@ -3,46 +3,48 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.db.models import Q
 
 from .models import Link, Report
+from .utils import get_username
 
 def sites_list(request):
 
     context = {
-                'links':Link.objects.all(),
+                'links':Link.objects.filter(is_verified=True),
             }
 
     return render(request, 'sources/index.html',  context=context)
 
-def contact_form(request):
+def contact_form(request, id=None):
     form = request.path.split("/")[2]
-    context = {'name': form}
+    context = {'context': form}
+    if form == 'report':
+        context.update({'item':Link.objects.get(id=id)})
     return render(request, f'sources/{form}.html', context=context)
 
-# def add(request, context):
-#     response = redirect('sources:links')
+def add(request, context):
+    response = redirect('sources:')
 
-#     def add_report():
-#         q = request.POST.get('site-name')
-#         p = request.POST.get('problem')
-#         try:
-#             link = Link.objects.filter(Q(site_name__contains=q)|Q(site_address__contains=q))[0]
-#             Report(site=link, problem=p, author=get_username(request)).save()
-#         except:
-#             pass
+    def add_report():
+        problem = request.POST.get('problem')
+        id = request.POST.get('id')
+        link = Link.objects.get(id=id)
+        Report(site=link, problem=problem, author=get_username(request)).save()
 
-#     def add_suggest():
-#         Link(site_name=request.POST.get('site-name'),
-#              site_discription=request.POST.get('site-discription'),
-#              site_address=request.POST.get('site-address'),
-#              author=get_username(request),
-#              is_verified=False
-#             ).save()
+    def add_suggest():
+        print(request.POST.get('site-name'))
+        Link(site_name=request.POST.get('site-name'),
+             site_discription=request.POST.get('site-discription'),
+             site_address=request.POST.get('site-address'),
+             author=get_username(request),
+             is_verified=False
+            ).save()
 
-#     if context == "report":
-#         add_report()
-#         return response
+    if context == "report":
+        add_report()
+        return response
 
-#     add_suggest()
-#     return response
+    if context == "suggest":
+        add_suggest()
+        return response
 
 def download(request):
     response = HttpResponse(content_type='text/csv')
