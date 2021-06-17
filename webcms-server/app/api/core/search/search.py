a910c14eff9c61758f2dda8e55b1323960b61381
Jakob OGANESYAN <jakoganesyan@gmail.com>
2021-06-17 15:12:02 +0200

    refactor search

--- webcms-server/app/api/core/search/search.py
+++ webcms-server/app/api/core/search/search.py
@@ -1,26 +1,40 @@
 from re import match
 
 from django.db.models import Q
+from django.db.models.query import QuerySet, EmptyQuerySet
 
-from api.models import *
+from django.http.request import HttpRequest
 
-from .interface import Isearch
+from api.core.service.interface import Service
+from api.models import CoordinateReferenceSystem
 
-class CoordinateReferenceSystemSearch(Isearch):
+class CoordinateReferenceSystemSearch(Service):
 
-    def __init__(self, request):
-        
-        if match("\W + \S", request) or len(request)==0:
-            self.find = []
-            return
+    def _get_result(self) -> QuerySet:
+        if match("\W + \S", self._query) or match("[^\S]", self._query) or len(self._query)==0:
+            return EmptyQuerySet
 
-        if request.isdigit() and len(request) >= 4:
-            self.find = CoordinateReferenceSystem.objects.filter(coord_ref_sys_code=request)
-            return
+        if self._query.isdigit() and len(self._query) >= 4:
+            return CoordinateReferenceSystem.objects.filter(coord_ref_sys_code=self._query)
 
-        q = fr'\y({request})\y'
-        self.find = CoordinateReferenceSystem.objects.filter(
+        q = fr'\y({self._query})\y'
+        return CoordinateReferenceSystem.objects.filter(
                                                                 Q(coord_ref_sys_name__iregex=q) |
                                                                 Q(area_name__iregex=q) |
                                                                 Q(area__area_of_use__iregex=q)
                                                             )
+    def _get_queryset(self, request:HttpRequest) -> str:
+        return request.GET.get('q')
+
+    def _get_response(self) -> dict:
+        get_item = lambda item:{
+                    'code':item.coord_ref_sys_code,
+                    'name':item.coord_ref_sys_name,
+                    'area':item.area_name,
+                    }
+        find_crs = list(map(get_item, self._result))
+        return  {
+                    'epsg_exist': True,
+                    'find': len(self._result),
+                    'findCRS': find_crs,
+                }
--- webcms-server/app/api/views.py
+++ webcms-server/app/api/views.py
@@ -7,17 +7,17 @@ from rest_framework.response import Response
 
 from .core.converter.interface import PointConversionInterface
 from .core.search.search import CoordinateReferenceSystemSearch
+from .core.service.interface import Service
 
 from .models.Popular import Popular
 from .interfaces import CoordinateReferenceSystemInterface
-
 def render_page(func):
     def wrapper(self, request):
-        # try:
+        try:
             responce = func(self=self, request=request)
             return Response(responce)
-        # except Exception as e:
-        #     return HttpResponse(e, status=404)
+        except Exception as e:
+            return HttpResponse(e, status=404)
     return wrapper
 
 class About(APIView):
@@ -65,30 +65,11 @@ class Search(APIView):
 
     @render_page
     def get(self, request, format=None):
-        if ((qwest:=request.GET.get('q'))==None): 
-            raise Exception ("Can't find anything for empty request")
-        
-        self.context = CoordinateReferenceSystemSearch(qwest)
-        return self.__get_responce()
-
-    def __get_responce(self) -> dict:
         try:
-            return  {
-                        'epsg_exist': True,
-                        'find': len(self.context),
-                        'findCRS': self.__get_crs_list(),
-                    }
+            return CoordinateReferenceSystemSearch(request).get()
         except:
             return {
-                        'epsg_exist': False,
-                    }
-
-    def __get_crs_list(self) -> dict:
-        for i in self.context.get_list_of_find():
-            yield {
-                    'code':i.coord_ref_sys_code,
-                    'name':i.coord_ref_sys_name,
-                    'area':i.area_name,
+                    'epsg_exist': False,
                 }
 
 class Globe(APIView):
@@ -104,15 +85,10 @@ class PopularCRS(APIView):
     
     @render_page
     def get(self, request, format=None):
-        return self.__get_responce()
-
-    def __get_responce(self) -> dict:
-        return {self.__get_crs_list()}
-    
-    def __get_crs_list(self) -> dict:
-        for i in Popular.objects.all():
-            yield {
-                    'code':i.crs.coord_ref_sys_code,
-                    'name':i.crs.coord_ref_sys_name,
-                    'area':i.crs.area_name,
+        get_item = lambda item:{
+                    'code':item.crs.coord_ref_sys_code,
+                    'name':item.crs.coord_ref_sys_name,
+                    'area':item.crs.area_name,
                 }
+        find_popular_crs = list(map(get_item, Popular.objects.all()))
+        return [find_popular_crs]
