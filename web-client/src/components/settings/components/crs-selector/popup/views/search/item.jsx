c5dd3a2484bb9ebcf77c48921fc2dccef0684acb
Jakob OGANESYAN <jakoganesyan@gmail.com>
2021-06-30 00:07:14 +0200

    fea item controllers

--- web-client/src/components/settings/components/crs-selector/popup/views/search/item.jsx
+++ web-client/src/components/settings/components/crs-selector/popup/views/search/item.jsx
@@ -1,13 +1,22 @@
+import { useState, memo } from 'react';
 import './item.css'
 
-function CrsItem({element, i, onClick=()=>null}){
+export default memo(function CrsItem({element, i, onClick=()=>null}){
+    const [isActive, toggleActive] = useState(false);
     return (
-        <div className='result__item' onClick={() => {onClick(element)}}  key={i}>
-            <h3 className='result__item-title'>{element.name}</h3>
-            <h4 className='result__item-description result__item-description--area'>Area: {element.area}</h4>
-            <h4 className='result__item-description result__item-description--code'>Code: {element.code}</h4>
+        <div className='result__item' key={i}>
+            <div className="result__item-about" onClick={() => toggleActive(!isActive)} >
+                <h3 className='result__item-title'>{element.name}</h3>
+                <h4 className='result__item-description result__item-description--area'>Area: {element.area}</h4>
+                <h4 className='result__item-description result__item-description--code'>EPSG Code: {element.code}</h4>
+            </div>
+            { (isActive) ?
+                <div className="result__item-control">
+                    <button className='result__item-select' onClick={()=>onClick(element.code)}>select</button>
+                    <button className='result__item-view'>view on globe</button>
+                    <button className='result__item-more'>more information about</button>
+                </div> : null
+            }
         </div>
     );
-}
-
-export default CrsItem;
+})
