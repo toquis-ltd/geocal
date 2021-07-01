ea696932aae3e452b644aa067322b1dcb0aa10e7
Jakob OGANESYAN <jakoganesyan@gmail.com>
2021-07-01 22:57:10 +0200

    init file upload front + view

--- webcms-server/app/sources/admin.py
+++ webcms-server/app/sources/admin.py
@@ -1,4 +1,10 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.contrib.auth.decorators import permission_required
+
+from django.utils.decorators import method_decorator
+from django.urls import path
+from django.shortcuts import render, redirect
+
 from .models import Link, Report
 
 def verify(modeladmin, request, queryset):
@@ -12,6 +18,7 @@ class LinkAdmin(admin.ModelAdmin):
     list_display = ('site_name', 'is_verified', 'author', 'date')
     ordering = ['is_verified']
     actions = [verify]
+    change_list_template = 'admin/sources/change_list.html'
 
     def get_form(self, request, obj=None, **kwargs):
         form = super(LinkAdmin, self).get_form(request, obj, **kwargs)
@@ -19,6 +26,24 @@ class LinkAdmin(admin.ModelAdmin):
         form.base_fields['is_verified'].initial = request.user.is_staff
         return form
 
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('import-csv/', self.import_csv, name='import-links'),
+        ]
+        return my_urls + urls
+
+    @method_decorator(permission_required("sources", login_url='admin:login'))
+    def import_csv(self, request):
+        if request.method == "POST":
+            if (csv_file := request.FILES.get("csv_file", None)) == None:
+                self.message_user(request, "Can't reache the file", level=messages.ERROR)
+                return redirect("..")
+            self.message_user(request, "Your csv file has been imported")
+            return redirect("..")
+        
+        return render(request, "admin/sources/csv_form.html")
+    
 class ReportAdmin(admin.ModelAdmin):
     search_fields = ['site_name', 'author']
     list_display = ('site', 'author', 'date')
