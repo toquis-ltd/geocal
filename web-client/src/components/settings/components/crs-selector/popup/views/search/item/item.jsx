9465df946e4f377b7d51bfc65585b4422f35722c
Jakob OGANESYAN <jakoganesyan@gmail.com>
2021-08-01 22:20:21 +0200

    search item new design

--- web-client/src/components/settings/components/crs-selector/popup/views/search/item/item.jsx
+++ web-client/src/components/settings/components/crs-selector/popup/views/search/item/item.jsx
@@ -1,13 +1,30 @@
-import { useState, memo } from 'react';
-import './item.css'
+import { useState, useEffect, useRef } from 'react';
 
-export default memo(function CrsItem({element, i, onClick=()=>null}){
+import './item.sass'
+
+
+export default function CrsItem({element, i, onClick=()=>null}){
     const [isActive, toggleActive] = useState(false);
+    const wraper =  useRef(null);
+    
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (isActive && wraper.current && !wraper.current.contains(event.target) ) {
+                toggleActive(false);
+            }
+        }
+        document.addEventListener("mouseup", handleClickOutside);
+        return () => {
+            document.removeEventListener("mouseup", handleClickOutside);
+        };
+    }, [wraper, isActive]);
+
     return (
-        <div className='result__item' key={i}>
+        <div className={`result__item ${isActive && 'result__item--activate'}`} key={i} ref={wraper}>
             <div className="result__item-about" onClick={() => toggleActive(!isActive)} >
                 <h3 className='result__item-title'>{element.name}</h3>
                 <h4 className='result__item-description result__item-description--area'>Area: {element.area}</h4>
+                <h4 className='result__item-description result__item-description--unity'>Unity: {element.unityOfMeasure}</h4>
                 <h4 className='result__item-description result__item-description--code'>EPSG Code: {element.code}</h4>
             </div>
             { isActive &&
@@ -15,8 +32,8 @@ export default memo(function CrsItem({element, i, onClick=()=>null}){
                     <button className='result__item-select' onClick={()=>onClick(element.code)}>select</button>
                     <button className='result__item-view'>view on globe</button>
                     <button className='result__item-more'>more information about</button>
-                </div>
+            </div>
             }
         </div>
     );
-})
+}
