a39da3016bba4ac98f414b9b265b6629a1934cd3
Jakob OGANESYAN <jakoganesyan@gmail.com>
2021-08-18 06:00:42 +0200

    fea auto filter

--- web-client/src/components/settings/crs-selector/popup/views/search/result/result.tsx
+++ web-client/src/components/settings/crs-selector/popup/views/search/result/result.tsx
@@ -1,5 +1,5 @@
+import { memo, useEffect, useState } from 'react';
 import { useSelector} from 'react-redux';
-import { memo } from 'react';
 
 import CrsItem from '../item/item';
 
@@ -8,11 +8,21 @@ import './result.sass';
 
 export default memo(function Result () {
     const result = useSelector(state => state.popups.result);
+    const origin = useSelector(state => state.settings.modifiedCRS);
+    const source = useSelector(state => state.settings.source);
+    const [find, handleFind] = useState(result?.findCRS)
+
+    useEffect(()=>{
+        if (origin === 'target') {
+            handleFind(result?.findCRS?.filter(item => item.code !== source.code))
+        };
+    }, []);
+
     return(
         <div className='result'>
                 <div className="result__inner">
                     {
-                        result?.findCRS?.map( elem => <CrsItem element={elem}/> )
+                        find?.map( elem => <CrsItem element={elem}/> )
                     }
                 </div>
         </div>
