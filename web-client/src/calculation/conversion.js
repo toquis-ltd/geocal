5f29f8b611f9cd4caa5d138d55ea4b66350358e7
Jakob OGANESYAN <jakoganesyan@gmail.com>
2021-09-01 19:49:23 +0200

    first build

--- web-client/src/calculation/conversion.js
+++ web-client/src/calculation/conversion.js
@@ -1,12 +1,17 @@
 import proj4 from 'proj4';
 
+import {convertToDecimal} from './format'
 
 export default function FetchConvertion(source, target, point) {
     let response;
     try {
-        const coordinate = Object.values(point).map(i => parseFloat(parseFloat(i).toFixed(8)))
+        const coordinate = Object.values(point)
+                                        .map(prop=>convertToDecimal(prop))
+                                        .map(i => parseFloat(parseFloat(i).toFixed(8)))
+                                        
         response = proj4(source, target, coordinate).map(item => item.toFixed(7).toString());
     } catch (error) {
+        console.log(error)
         response = ['-', '-', '-'];
     }
     return {x:response[0], y:response[1], z:response[2]}
--- web-client/src/components/converter/blocks/fields.jsx
+++ web-client/src/components/converter/blocks/fields.jsx
@@ -1,40 +1,29 @@
 import useCRSUnity from "hooks/useCRSUnity";
 
+function Field({className, name, point, readOnly, placeholder, onChange}) {
+    return (
+        <input 
+            className={`${className}-coordinate__field fields__item-field`}
+            onChange={onChange}
+            name={name}
+            value={point[name] || ""}
+            placeholder={placeholder}
+            autoComplete="off"
+            readOnly={readOnly}
+            type='tel'/>
+
+    )
+};
+
 export function Fields ({name, point, origin, onChange=()=>null}) {
     const unity = useCRSUnity(origin);
     const readOnly = name !== 'input'
     return (
         <div className={`${name}-fields__container fields__container`}>
-            <input 
-                className={`${name}-coordinate__field fields__item-field`}
-                onChange={onChange}
-                
-                name='x'
-                value={point.x || ""}
-                placeholder={unity[0]}
-                autoComplete="off"
-                readOnly={readOnly}
-                type='tel'/>
-            <input
-                className={`${name}-coordinate__field fields__item-field`} 
-                onChange={onChange}
-                
-                name='y'
-                value={point.y || ""}
-                placeholder={unity[1]}
-                autoComplete="off"
-                readOnly={readOnly}
-                type='tel'/>  
-            <input 
-                className={`${name}-coordinate__field fields__item-field`}
-                onChange={onChange}
-                
-                name='z'
-                value={point.z || ""}
-                placeholder={unity[2]}
-                autoComplete="off"
-                readOnly={readOnly}
-                type='tel'/>    
+            
+            <Field className={name} name='x' onChange={onChange} point={point} readOnly={readOnly} placeholder={unity[0]}/>
+            <Field className={name} name='y' onChange={onChange} point={point} readOnly={readOnly} placeholder={unity[1]}/>
+            <Field className={name} name='z' onChange={onChange} point={point} readOnly={readOnly} placeholder={unity[2]}/>
         </div>
         )
 }
