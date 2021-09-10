0062b2037cb6d9b9757a55aff6c3eb938b62b425
Jakob Oganesyan <jakoganesyan@gmail.com>
2021-09-10 18:07:35 +0200

    update front

--- webcms-server/entrypoint.sh
+++ webcms-server/entrypoint.sh
@@ -4,6 +4,6 @@ python manage.py makemigrations --noinput
 
 python manage.py migrate --noinput
 
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput 
 
 gunicorn GeoCal.wsgi --bind 0.0.0.0:8000
