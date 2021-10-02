9ba86251811390ad40d5719d19a04cd74644bf51
Jakob Oganesyan <jakoganesyan@gmail.com>
2021-10-02 10:17:28 +0200

    fix Decimal direction + fea DMS button + build

--- web-client/src/components/settings/button/global.jsx
+++ web-client/src/components/settings/button/global.jsx
@@ -4,14 +4,15 @@ import { useDispatch, useSelector } from "react-redux";
 export default function ConverterPropretiesBtn(){
     const dispatch = useDispatch();
     const zAxe = useSelector(({settings}) => settings.zAxe);
-    const onClick = () => dispatch(toggleZAxe(!zAxe));
+    const zAxeChange = () => dispatch(toggleZAxe(!zAxe));
 
     return (
         <div className='global-settings  selector__btn'>
             Settings
             <div className="global-settings__inner">
                 <ul className="propreties__list">
-                    <li>Z axe: <input onClick={onClick} onChange={()=>null} checked={zAxe} type='checkbox'/></li>
+                    <li>Z axe: <input onClick={zAxeChange} onChange={()=>null} checked={zAxe} type='checkbox'/></li>
+                    <li>DMS result: <input onChange={()=>null} checked={false} type='checkbox'/></li>
                 </ul>
             </div>
         </div>
