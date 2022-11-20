202e707f9e67dc5eae97d5689e3e694b30c0e2fa
jak <jakoganesyan@gmail.com>
2022-11-20 18:15:16 +0100

    fix

--- webcms-server/entrypoint.dev.sh
+++ webcms-server/entrypoint.dev.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+apt install -y wget
+
+wget https://cdn.proj.org/us_nga_egm08_25.tif -P /usr/local/lib/python3.8/dist-packages/pyproj/proj_dir/share/proj
+
 PROJ_DOWNLOAD_DIR=$(python3 -c "import pyproj; print(pyproj.datadir.get_user_data_dir())" )
 
 wget --mirror https://cdn.proj.org/ -P ${PROJ_DOWNLOAD_DIR}
--- webcms-server/entrypoint.sh
+++ webcms-server/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+apt install -y wget
+
+wget https://cdn.proj.org/us_nga_egm08_25.tif -P /usr/share/proj
+
 python3 manage.py makemigrations --noinput
 
 python3 manage.py migrate --noinput
